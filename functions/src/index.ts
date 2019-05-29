739d8beef579c8d566a4ddbf0209d89674f2f69e
Sam Edelsten <samedelsten1@gmail.com>
2019-05-29 12:30:52 +0100

    Room creation and deletion functions started

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,59 @@
+// TODO classes and interfaces as module
+
+interface ICard {
+    suit: string,
+    number: string
+}
+
+class Deck {
+    public cards: Array<ICard> = [];
+    constructor(private jokercount: number = 0) {
+        const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "JK"];
+        const suits = ["C", "D", "H", "S"];
+
+        for (let n of numbers) {
+            if (n != "JK") {
+                for (let s of suits) {
+                    this.cards.push({ suit: s, number: n });
+                }
+            } else {
+                for (let i = 0; i < this.jokercount; i++) {
+                    this.cards.push({ suit: n, number: n });
+                }
+            }
+        }
+    }
+}
+
+class RuleSet {
+    public rules: Object;
+    constructor() {
+        this.rules = {
+            "A": 'Waterfall',
+            "2": 'Choose',
+            "3": 'Me',
+            "4": 'Whores',
+            "5": 'ThumbMaster',
+            "6": 'Dicks',
+            "7": 'Heaven',
+            "8": 'Mate',
+            "9": 'Bust-a-Rhyme',
+            "10": 'Categories',
+            "J": 'Make a Rule',
+            "Q": 'QuestionMaster',
+            "K": 'Pour',
+            "JK": 'Travolta',
+        }
+    }
+}
+
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
+const cors = require('cors')({ origin: true });
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,31 +62,135 @@ export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
 
-export const helloAgain = functions.https.onRequest((req, res) => {
-    console.log('newhello');
-    res.send("Hello again from Sam.");
-});
+export const joinRoom = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const data = JSON.parse(req.body);
+
+        // TODO some sort of auth stuff
+
+        // Check provided room exists
+
+        const roomRef = db.collection('rooms').doc(data.roomId)
+
+        roomRef.get()
+            .then(snap => {
+                const doc = snap.data();
+                if (!snap.exists) {
+                    return Promise.reject('joinRoom: Room not found!');
+                }
+
+                if (!doc.pin) {
+                    return Promise.reject('joinRoom: Room has no pin!');
+                }
+
+                if (doc.pin == data.pin) {
+                    return Promise.resolve();
+                }
+
+                return Promise.reject('joinRoom: Incorret PIN!');
+            })
+            .then(() => {
+                // add player to room
+                return db.runTransaction(t => {
+                    return t.get(roomRef)
+                        .then(roomDoc => {
+                            if (!roomDoc.exists) {
+                                return Promise.reject('joinRoom: Room doesn\'t exist!')
+                            }
+
+                            let players = roomDoc.data().players;
+
+                            if (!players[data.user.uid]) {
+
+                                players[data.user.uid] = data.user;
+
+                                t.update(roomRef, { players: players });
+                                return Promise.resolve();
+                            } else {
+                                return Promise.reject('joinRoom: Player already in room!')
+                            }
+                        })
+                })
+            })
+            .then(() => res.send({ joined: true }))
+            .catch(error => res.send({ error, joined: false }))
+    });
+})
+
+// TODO on room update: change modified timestamp
+// REMEMBER TO NOT MAKE AN INFINITE LOOP HERE SAM!!!!!!!!
 
 // on new room created
 // populate room with owner, deck, rules
 export const roomCreated = functions.firestore
-    .document('test/{testId}')
-    .onCreate((snap, ctx) => {
-        console.log("ROOM CREATED");
-        console.log(ctx.params.testId);
+    .document('rooms/{roomId}')
+    .onCreate(async (snap, ctx) => {
+        const roomId = ctx.params.roomId;
+
+        function createPin(len) {
+            let pin: string = '';
+            for (let i = 0; i < len; i++) {
+                pin = pin + Math.floor(Math.random() * 10).toString();
+            }
+            return pin;
+        }
+
+        if (roomId !== 'roomsinfo') {
+            const deck = new Deck();
+            const rules = new RuleSet();
+            const pin = createPin(4);
+            const infoRef = snap.ref.parent.doc('roomsinfo');
+
+            infoRef.set({ roomlist: {[roomId]:false} }, { merge: true });
+
+            await db.runTransaction(t => {
+                return t.get(infoRef).then(infoDoc => {
+                    if (!infoDoc.exists) {
+                        throw "No roomsinfo!";
+                    }
+
+                    // let newCount = infoDoc.data().roomcount + 1;
+                    t.update(infoRef, { roomcount: admin.firestore.FieldValue.increment(1) });
+                })
+            }).then(() => { console.log('Transaction roomcount done') })
+                .catch(e => console.error);
+
+            snap.ref.set({
+                deck: deck.cards,
+                rules: rules.rules,
+                currentCard: '',
+                pin: pin,
+                players: {},
+                turnOrder: [],
+                timestamp: {
+                    created: admin.firestore.FieldValue.serverTimestamp(),
+                    modified: admin.firestore.FieldValue.serverTimestamp()
+                }
+            }, { merge: true })
+                .then(() => { console.log('Added deck, rules, currentCard, pin, turnOrder.') }, e => console.error)
+        }
     });
 
-export const testUpdate = functions.firestore
-    .document('test/{testId}')
-    .onUpdate((change, ctx) => {
-        console.log("SOMETHING UPDATED");
-        console.log(ctx.params.testId);
-    });
+export const roomDeleted = functions.firestore
+    .document('rooms/{roomId}')
+    .onDelete(async (snap, ctx) => {
+        const roomId = ctx.params.roomId;
 
-    export const testWrite = functions.firestore
-    .document('test/{testId}')
-    .onWrite((change, ctx) => {
-        console.log("SOMETHING WRITTEN");
-        console.log(ctx.params.testId);
-        return true;
+        if (roomId !== 'roomsinfo') {
+            const infoRef = snap.ref.parent.doc('roomsinfo');
+
+            infoRef.set({ roomlist: {[roomId]:false} }, { merge: true });
+
+            await db.runTransaction(t => {
+                return t.get(infoRef).then(infoDoc => {
+                    if (!infoDoc.exists) {
+                        throw "No roomsinfo!";
+                    }
+
+                    // let newCount = infoDoc.data().roomcount - 1;
+                    t.update(infoRef, { roomcount: admin.firestore.FieldValue.increment(-1) });
+                })
+            }).then(() => { console.log('Transaction roomcount done') })
+                .catch(e => console.error);
+        }
     });
--- ts/index.ts
+++ ts/index.ts
@@ -58,8 +58,8 @@ async function authHandler(user: any) {
         // console.log(user);
 
         userdata.populateFrom(user.uid)
-        .then(userExists => { userExists && userdata.name ? openPage('home') : openPage('account') },
-              e => { console.error(e) });
+            .then(userExists => { userExists && userdata.name ? openPage('home') : openPage('account') },
+                e => { console.error(e) });
     } else {
         // logged out
         userdata = new UserData; //clear user info
@@ -67,10 +67,100 @@ async function authHandler(user: any) {
     }
 }
 
-async function joinRoom(roomId:string,roomPass:string) {
+async function joinRoom(roomId: string, roomPass: string) {
     console.log(`TODO join room function. ID:${roomId} PW:${roomPass}`);
     // attempt to write user info to room
     // cloud function returns acceptance/rejection
     // > acceptance is in for of room info, we then set up a live listen to updates in room
     // > rejection is null, error out here
+}
+
+async function getRoomId(len: number = 4) {
+    const roomsinfo = await db.collection('rooms').doc('roomsinfo').get()
+        .then(doc => {
+            if (doc.exists) {
+                return doc.data();
+            } else {
+                return doc.ref.set({ roomcount: 0 })
+                    .then((e) => { return e })
+            }
+        })
+
+    function createId(len) {
+        let str = '';
+        for (let i = 0; i < len; i++) {
+            str = str + String.fromCharCode(Math.ceil((Math.random()) * 26) + 64);
+        }
+        // return 'test';
+        return str;
+    }
+
+    let newRoomId = createId(len);
+    let breakCounter = 0;
+
+    while ((roomsinfo.roomlist[newRoomId] != undefined) && (roomsinfo.roomlist[newRoomId] != false) && (breakCounter < 26 ** len)) {
+        newRoomId = createId(len);
+        breakCounter++;
+    }
+
+    if (breakCounter >= 26 ** len) {
+        //randomness has failed us!
+        // TODO sequentially check for available rooms here
+        return Promise.reject('Room slots full!')
+    }
+
+    return newRoomId;
+}
+
+async function tCreateRoom() {
+    return getRoomId()
+        .then(roomId => {
+            let roomDocRef = db.collection('rooms').doc(roomId);
+
+            return db.runTransaction(t => {
+                return t.get(roomDocRef).then(roomDoc => {
+                    if (!roomDoc.exists) {
+                        return t.set(roomDocRef, { owner: userdata.uid }, { merge: true });
+                    } else {
+                        throw `Room ${roomId} already exists!`
+                    }
+                })
+            })
+        })
+}
+
+async function requestJoinRoom(user: UserData = userdata, roomId: string, pin: string) {
+    if (!roomId) return Promise.reject('requestJoinRoom: No Room ID provided!');
+    if (!pin) return Promise.reject('requestJoinRoom: No Room PIN provided!');
+    if (!user) return Promise.reject('requestJoinRoom: No user provided!');
+
+    let data = { user, pin, roomId }
+
+    // TODO rate limiting to prevent bruteforce room entry
+    // as it would only take 26^4*10000 attempts to find any single room
+    // also even pentesting that would far exceed my quotas
+    // i need a revenue stream fffffffffffffff
+
+    fetch('https://us-central1-ring-of-fire-5d1a4.cloudfunctions.net/joinRoom', {
+        method: 'POST',
+        mode:'cors',
+        body: JSON.stringify(data)
+    })
+    .then(res=>{return res.json()})
+    .then(data=>console.log(data));
+
+    // Send data to cloud function to compare PIN
+    // TODO setup security to prevent room snooping from non owners
+    // > and people that haven't joined yet
+
+
+
+}
+
+async function deleteAllRooms() {
+    db.collection('rooms').get().then(qSnap => {
+        qSnap.forEach(doc => {
+            if (doc.id !== 'roomsinfo') console.log(doc.ref.delete());
+        })
+    })
 }
