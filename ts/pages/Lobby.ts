4ad3b34355ccd09e0f18bd20187d555404e4e704
Sam Edelsten <samedelsten1@gmail.com>
2019-06-06 20:44:12 +0100

    Added ready button

--- ts/pages/Lobby.ts
+++ ts/pages/Lobby.ts
@@ -31,5 +31,19 @@ class LobbyPage extends Page {
         let playerInfo = document.createElement('ce-player-list');
         this.page.appendChild(playerInfo);
 
+        let readyButton = document.createElement('button');
+        readyButton.textContent = room.data.players[userdata.uid].ready?'UnReady':'Ready';
+
+        readyButton.addEventListener('click', async e=>{
+            e.preventDefault();
+            let readiness = room.data.players[userdata.uid].ready;
+            // Update readiness in database
+            await db.collection('rooms').doc(room.roomId).set({players:{[userdata.uid]:{ready:!readiness}}}, {merge:true});
+            readyButton.textContent = readiness?'Ready':'UnReady';
+
+        })
+
+        this.page.appendChild(readyButton);
+
     }
 }
