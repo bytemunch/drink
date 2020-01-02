f2e906dcdc551ae05b16bed5c65e31fc5e72dd25
Sam Edelsten <samedelsten1@gmail.com>
2020-01-02 10:20:12 +0000

    Moved avatar functions into CeAvatar.ts

--- src/ts/elements/CeAvatar.ts
+++ src/ts/elements/CeAvatar.ts
@@ -24,18 +24,20 @@ class CeAvatar extends UpdateableElement {
     }
 
     set uid(uid) {
-        getColor(uid)
-        .then(color => {
-            this.color = color;
-            this.update();
-        })
-        getAvatar(uid)
-            .then(url => {
-                this.src = `url(${url})`;
-                this.update();
+        this._uid = uid;
+
+        let colorPromise = this.getColor()
+            .then(color => {
+                this.color = color;
             })
 
-        this._uid = uid;
+
+        let imgPromise = this.getAvatar()
+            .then(url => {
+                this.src = `url(${url})`;
+            })
+
+        Promise.all([colorPromise,imgPromise]).then(()=>this.update()).catch(e=>console.error(e));
     }
 
     set ready(bool) {
@@ -47,6 +49,24 @@ class CeAvatar extends UpdateableElement {
             this.style.borderTopColor = palette.red;
             this.style.borderBottomColor = palette.red;
         }
+    }
+
+    async getAvatar() {
+        const aviRef = firebase.storage().ref().child(`avatars/${this._uid}.png`);
+
+        let aviImg = await aviRef.getDownloadURL()
+            .catch(e => {
+                //console.error(e);
+                return '/img/noimg.png';
+            })
+        return aviImg;
+    }
+
+    async getColor() {
+        const userDoc = await firestore.collection('users').doc(this._uid).get()
+        const userData = await userDoc.data();
+
+        return userData.color;
     }
 
     update() {
--- src/ts/functions/getColor.ts
+++
@@ -1,7 +0,0 @@
-async function getColor(uid) {
-    const userDoc = await firestore.collection('users').doc(uid).get()
-    const userData = await userDoc.data();
-
-    return userData.color;
-
-}
